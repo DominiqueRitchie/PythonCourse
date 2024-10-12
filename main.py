54ed841339811bb026b210165d2ab83ef863ab05
DominiqueRitchie <dominique.ritchie@outlook.com>
2024-10-12 20:36:56 +0400

    Ввод и вывод данных (12.10.2024).

---
+++ main.py
@@ -0,0 +1,15 @@
+"""basic types of data + names of variables"""
+
+a_int = int(5.4)  # целочисленный тип данных
+b_float = float(4.8)  # дробный тип данных
+c_str = "Hello"  # строковый тип данных
+
+user_input_str = input("Enter your text: ")
+user_input_int = int(input("Enter a whole number: "))
+user_input_float = float(input("Enter a fraction: "))
+
+print()
+print(a_int, b_float, c_str)
+print(user_input_str, user_input_int, user_input_float)
+print("Dominique" * 5)
+print("Dominique " + "Angry >:(")  # конкатенация (сложение строк)
