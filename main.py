d8881bb5964dc38a0233213b22826a462f14f80a
DominiqueRitchie <dominique.ritchie@outlook.com>
2024-10-12 21:19:39 +0400

    This is test

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 """basic types of data + names of variables"""
 
-a_int = int(5.4)  # целочисленный тип данных
-b_float = float(4.8)  # дробный тип данных
-c_str = "Hello"  # строковый тип данных
+a_int = int(5.4)  # целочисленный тип данных / whole number
+b_float = float(4.8)  # дробный тип данных / fraction
+c_str = "Hello"  # строковый тип данных / string
 
 user_input_str = input("Enter your text: ")
 user_input_int = int(input("Enter a whole number: "))
 user_input_float = float(input("Enter a fraction: "))
 
 print()
-print(a_int, b_float, c_str)
+print(a_int, b_float, c_str)        
 print(user_input_str, user_input_int, user_input_float)
 print("Dominique" * 5)
 print("Dominique " + "Angry >:(")  # конкатенация (сложение строк)
-###
+# This is test from VSC
