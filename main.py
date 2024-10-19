cefad2ceb063d4cef62c4b2de91925de4b44a867
DominiqueRitchie <dominique.ritchie@outlook.com>
2024-10-19 20:14:08 +0400

    Ввод и вывод данных (12.10.2024). Интерполяция + Выравнивание (19.10.2024)

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 """basic types of data + names of variables"""
 
-a_int = int(5.4)  # целочисленный тип данных
-b_float = float(4.8)  # дробный тип данных
-c_str = "Hello"  # строковый тип данных
+#a_int = int(5.4)  # целочисленный тип данных
+#b_float = float(4.8)  # дробный тип данных
+#c_str = "Hello"  # строковый тип данных
 
-user_input_str = input("Enter your text: ")
-user_input_int = int(input("Enter a whole number: "))
-user_input_float = float(input("Enter a fraction: "))
+#user_input_str = input("Enter your text: ")
+#user_input_int = int(input("Enter a whole number: "))
+#user_input_float = float(input("Enter a fraction: "))
 
-print()
-print(a_int, b_float, c_str)
-print(user_input_str, user_input_int, user_input_float)
-print("Dominique" * 5)
-print("Dominique " + "Angry >:(")  # конкатенация (сложение строк)
-###
+#print()
+#print(a_int, b_float, c_str)
+#print(user_input_str, user_input_int, user_input_float)
+#print("Dominique" * 5)
+#print("Dominique " + "Angry >:(")  # конкатенация (сложение строк)
+
+"""Домашка"""
+# print("hello " + "world")
+# print("hello" * 5)
+# print(int("7") + int("5"))
+# print("The answer is " + "5")
+# print("goodbye" * 3)
+# print("no " + str(26))
+# #print("sos" * 8)
+# print(float("7.7"))
+# print(int("6"))
+# print(2 + 7)
+# print(2.7 * 3.5)
+# print(int(7.9))
+# print(int(7.8 / 9.4))
+# print(3.3 + 9)
+# print("yes " + str(5.7))
+# print(int(5 / 3))
+
+"""Урок 2   19.10.2024. Интерполяция + Выравнивание"""
+
+# user_name = input("Enter your name: ")
+# print("Hello,", user_name)
+# print(f"Hello, {user_name}!")  # Интерполяция (f строка)
+# print(f"\"")  # \ - экранирующий символ
+# print(f"{2024:->9}")
+# print(f"{123: <9}")
+# print(f"{80708:-^7}")
+# print("hello\nyour name is:", user_name)  # \n — переход на новую строку
